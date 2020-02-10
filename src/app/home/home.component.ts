817f9c5f6912298f790b750b5005e675e0c7ef2d
chedly <chedly.mar@gmail.com>
2020-02-10 11:00:14 +0100

    finish navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { emptyScheduled } from 'rxjs/internal/observable/empty';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,14 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  employeeMenu:boolean=false;
+  
   ngOnInit() {
+  }
+
+  toggleEmployeeMenu() :void{
+    this.employeeMenu=!this.employeeMenu;
+    console.log(this.employeeMenu);
   }
 
 }
